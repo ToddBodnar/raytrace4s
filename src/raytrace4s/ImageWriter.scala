e87edab750a309ba1ac3a2778930db3dfac7e301
ToddBodnar <toddbodnar@gmail.com>
2016-12-25 21:18:38 -0800

    lazy eval the stream

--- src/raytrace4s/ImageWriter.scala
+++ src/raytrace4s/ImageWriter.scala
@@ -4,7 +4,7 @@ import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
 import java.io.File
 
-class ImageWriter(width: Integer, height: Integer, pixels: Stream[Pixel]) {
+class ImageWriter(width: Integer, height: Integer, pixels: => Stream[Pixel]) {
   def img = {
     val result = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
     pixels.foreach { pixel =>
