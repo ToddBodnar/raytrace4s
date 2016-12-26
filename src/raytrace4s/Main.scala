84811bca72914cd484b20be6b8c0ac0cb3df9eb1
ToddBodnar <toddbodnar@gmail.com>
2016-12-26 02:14:57 -0800

    reflection

--- src/raytrace4s/Main.scala
+++ src/raytrace4s/Main.scala
@@ -9,13 +9,17 @@ object Main extends App {
   val width = 1000
   val height = 500
 
-  val subsamples = 20
+  val subsamples = 2
   val maxsteps = 10
 
-  val shapes = List(new Sphere(new Vector3d(0, 0, -1), 0.5, new Material(new Color(1, 0, 0), 0.25,0.5)),
-    new Sphere(new Vector3d(0.35, 0, -.75), 0.35, new Material(new Color(0.1, 0.1, 0.1), 0.25,0.5)),
-    new Sphere(new Vector3d(-0.35, 0.35, -1.05), 0.35, new Material(new Color(0, 1, 0), 0.25,0.5)),
-    new Sphere(new Vector3d(0, -10000.5, -1), 10000, new Material(new Color(1, 1, 0), 0.25,0.5)))
+  val shapes = List(new Sphere(new Vector3d(0, 0, -1), 0.5, new Material(new Color(1, 0, 0), 0.25,0.5,0,0)),
+    new Sphere(new Vector3d(0.35, 0, -.75), 0.35, new Material(new Color(0.1, 0.1, 0.1), 0.25,0.5,0,0)),
+    new Sphere(new Vector3d(-0.35, 0.35, -1.05), 0.35, new Material(new Color(0, 1, 0), 0.25,0.5,0,0)),
+    new Sphere(new Vector3d(-1.5, -0.25, -1.05), 0.15, new Material(new Color(.9, .9, .9), 0.25,0.5,.5,.25)),
+    new Sphere(new Vector3d(-1.5, 0.05, -1.05), 0.15, new Material(new Color(.9, .9, .9), 0.25,0.5,1,.25)),
+    new Sphere(new Vector3d(-1.5, 0.35, -1.05), 0.15, new Material(new Color(.9, .9, .9), 0.25,0.5,.5,0)),
+    new Sphere(new Vector3d(-1.5, 0.65, -1.05), 0.15, new Material(new Color(.9, .9, .9), 0.25,0.5,1,0)),
+    new Sphere(new Vector3d(0, -10000.5, -1), 10000, new Material(new Color(1, 1, 0), 0.25,0.5,0,0)))
 
   new ImageWriter(width, height, new WorldRenderer(width, height, new World(shapes), subsamples, maxsteps)).write("test")
 }
--- src/raytrace4s/primitives/Material.scala
+++ src/raytrace4s/primitives/Material.scala
@@ -1,6 +1,6 @@
 package raytrace4s.primitives
 
-class Material(baseColor: Color, diffuseAmt: Double, lightDampening: Double) {
+class Material(baseColor: Color, diffuseAmt: Double, lightDampening: Double, reflectionAmount: Double, reflectionFuzzy: Double) {
   def getColor(ray: Ray, distance: Double, normal: Vector3d, tracer: (Ray, Int) => Color, bounces: Int): Color = {
     if (bounces <= 0) {
       new Color(0, 0, 0)
@@ -14,8 +14,20 @@ class Material(baseColor: Color, diffuseAmt: Double, lightDampening: Double) {
           p
         }
       }
+      
+      def reflect(incomming: Vector3d, normal: Vector3d): Vector3d = {
+        incomming - (normal * ((incomming dot normal) * 2))
+      }
+      
       val target = ray.pointAt(distance) + normal + getP
-      new Color((new Color(0,0,0).merge(tracer(new Ray(ray.pointAt(distance), target - ray.pointAt(distance)), bounces - 1), diffuseAmt).vector*lightDampening).sqrt*baseColor.vector)
+      val diffusedColor = new Color((new Color(0,0,0).merge(tracer(new Ray(ray.pointAt(distance), target - ray.pointAt(distance)), bounces - 1), diffuseAmt).vector*lightDampening).sqrt*baseColor.vector)
+      
+      if(reflectionAmount == 0){
+        diffusedColor
+      }else{
+        val mirrored = diffusedColor.merge(tracer(new Ray(ray.pointAt(distance),reflect(ray.direction, normal) + (getP * reflectionFuzzy)), bounces - 1), 1 - reflectionAmount)
+        mirrored
+      }
     }
   }
 }
