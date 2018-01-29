6620f9bbe9df846f9107dc3cba162b20b593ed88
ToddBodnar <toddbodnar@gmail.com>
2018-01-29 00:53:55 -0800

    better material defaults

--- src/main/scala/raytrace4s/primitives/MaterialFactory.scala
+++ src/main/scala/raytrace4s/primitives/MaterialFactory.scala
@@ -27,9 +27,9 @@ object MaterialFactory {
     }
   }
   def basicMaterial(color:Color): Material = {
-    new Material(new ColorTexture(color), 0.25, 0.25,0,0,0,0)
+    new Material(new ColorTexture(color), 0.25, 0.85,0,0,0,0)
   }
   def basicMaterial(texture:Texture): Material = {
-    new Material(texture, 0.25, 0.25,0,0,0,0)
+    new Material(texture, 0.25, 0.85,0,0,0,0)
   }
 }
